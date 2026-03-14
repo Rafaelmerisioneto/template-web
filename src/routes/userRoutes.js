a204d76135c6f62c300ea4bbde4cc18da78263ae
Jonathan Bufon <jonathanbufon@gmail.com>
2026-03-13 21:11:22 -0300

    feat(base-api) criar mocks, inicio da api

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -10,10 +10,10 @@ import {
 const router = express.Router();
 
 // Definindo as rotas
-router.get('/', getAllUsers);
-router.get('/:id', getUserById);
-router.post('/', createUser);
-router.put('/:id', updateUser);
-router.delete('/:id', deleteUser);
+router.get('/get-by-title', getByTitle);
+router.get('/get-by-date', getByDate);
+// router.get('/get-by-date', getUserById);
+// router.get('/get-by-date', getUserById);
+router.get('/get-by-category', getByCategory);
 
 export default router;
